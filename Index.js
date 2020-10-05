a97414dc98000a4901948cab68069fe44d44cc39
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-05 00:49:28 -0500

    engineer prompt

--- Index.js
+++ Index.js
@@ -1,1 +1,70 @@
 const inquirer = require("inquirer");
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const employees = [];
+
+function engineerData() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name:',
+          validate: (name) => {
+            if (name) {
+              return true;
+            } else {
+              console.log('This question requires an answer.');
+              return false;
+            }
+          },
+        },
+        {
+          type: 'list',
+          name: 'role',
+          message: 'You are the Manager.',
+          choices: ['Manager'],
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Please enter your employee ID number:",
+          validate: (id) => {
+            if (id) {
+              return true;
+            } else {
+              console.log("This question requires an answer.");
+              return false;
+            }
+          },
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "What is your email address:",
+          validate: (email) => {
+            if (email) {
+              return true;
+            } else {
+              console.log('This question requires an answer.');
+              return false;
+            }
+          },
+        },
+        {
+          type: 'input',
+          name: 'officeNumber',
+          message: 'What is your office number:',
+          validate: (officeNumber) => {
+            if (officeNumber) {
+              return true;
+            } else {
+              console.log('This question requires an answer.');
+              return false;
+            }
+          },
+        },
+      ]);
